68dc10dd7b70b65c211308c2d4478899e3754680
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-03 16:16:02 +0900

    ServerUtil에 로그인 수행 함수 작업중   - 호출 코드 추가   - 응답 코드 필요

--- app/src/main/java/com/nepplus/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/colosseum/utils/ServerUtil.kt
@@ -1,11 +1,37 @@
 package com.nepplus.colosseum.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
+
+
+
     companion object {
-//        static 변수 / 함수
 
+//        호스트 주소를 변수로 저장
+        private val HOST_URL = "http://54.180.52.26"
 
+//        로그인 기능 함수
+        fun postRequestSignIn(id: String, pw: String) {
+
+            val urlString = "${HOST_URL}/user";
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
+            val client = OkHttpClient()
+            client.newCall(request)
+
+        }
 
     }
 
