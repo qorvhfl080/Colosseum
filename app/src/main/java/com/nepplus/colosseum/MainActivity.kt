fc81776ec6e48d846488c929a2effc1b0c1f9b61
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-06 15:36:39 +0900

    토픽데이터 생성자 추가

--- app/src/main/java/com/nepplus/colosseum/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nepplus.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.colosseum.datas.TopicData
 import com.nepplus.colosseum.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,14 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
 
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
 
+                for (i in 0 until  topicsArr.length()) {
+                    val topicObj = topicsArr.getJSONObject(i)
+
+
+                }
 
             }
         })
--- app/src/main/java/com/nepplus/colosseum/datas/TopicData.kt
+++ app/src/main/java/com/nepplus/colosseum/datas/TopicData.kt
@@ -2,6 +2,8 @@ package com.nepplus.colosseum.datas
 
 class TopicData(var id: Int, var title: String, var imageURL: String) {
 
+    constructor() : this(0, "제목없음", "")
 
+    constructor(id: Int) : this(id, "제목없음", "")
 
 }
