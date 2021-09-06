d6166b0b6b68ae66b854bae9befdd8a8c18a8498
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-06 16:06:58 +0900

    자동 로그인

--- app/src/main/java/com/nepplus/colosseum/SplashActivity.kt
+++ app/src/main/java/com/nepplus/colosseum/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.nepplus.colosseum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.nepplus.colosseum.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,25 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        var myIntent: Intent
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            //        자동 로그인 여부 판단
+            if (ContextUtil.getAutoLogin(mContext) && ContextUtil.getToken(mContext) != "") {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+                myIntent = Intent(mContext, SignInActivity::class.java)
+            }
+
+            startActivity(myIntent)
+            finish()
+
+        }, 2500)
+
+
+
 
     }
 }
