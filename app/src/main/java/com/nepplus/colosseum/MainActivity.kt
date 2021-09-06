8c098d41424f142bdcecde3a0a259bc1c4210f00
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-06 11:45:04 +0900

    메인화면 새로 생성

--- app/src/main/java/com/nepplus/colosseum/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.nepplus.colosseum
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Toast
-import com.nepplus.colosseum.databinding.ActivityMainBinding
-import com.nepplus.colosseum.utils.ServerUtil
-import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -19,51 +11,9 @@ class MainActivity : BaseActivity() {
         setupEvents()
         setValues()
 
-
     }
 
     override fun setupEvents() {
-
-
-        signInBtn.setOnClickListener {
-
-            val inputId = emailEdt.text.toString()
-            val inputPw = passwordEdt.text.toString()
-
-            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
-                override fun onResponse(jsonObj: JSONObject) {
-
-                    val code = jsonObj.getInt("code")
-                    Log.d("server", code.toString())
-
-                    if (code == 200) {
-
-                        val dataObj = jsonObj.getJSONObject("data")
-                        val userObj = dataObj.getJSONObject("user")
-                        val nickname = userObj.getString("nick_name")
-
-                        runOnUiThread {
-                            Toast.makeText(mContext, "${nickname}님 ${jsonObj.getString("message")}", Toast.LENGTH_SHORT).show()
-                        }
-
-                    } else {
-
-                        runOnUiThread {
-                            Toast.makeText(mContext, "${jsonObj.getString("message")}", Toast.LENGTH_SHORT).show()
-                        }
-
-                    }
-                }
-            })
-
-        }
-
-        signUpBtn.setOnClickListener {
-
-            val myIntent = Intent(mContext, SignUpActivity::class.java)
-            startActivity(myIntent)
-
-        }
 
     }
 
---
+++ app/src/main/java/com/nepplus/colosseum/SignInActivity.kt
@@ -0,0 +1,70 @@
+package com.nepplus.colosseum
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.nepplus.colosseum.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.json.JSONObject
+
+class SignInActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        setupEvents()
+        setValues()
+
+
+    }
+
+    override fun setupEvents() {
+
+
+        signInBtn.setOnClickListener {
+
+            val inputId = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+                    Log.d("server", code.toString())
+
+                    if (code == 200) {
+
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "${nickname}님 ${jsonObj.getString("message")}", Toast.LENGTH_SHORT).show()
+                        }
+
+                    } else {
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "${jsonObj.getString("message")}", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                }
+            })
+
+        }
+
+        signUpBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+
+    override fun setValues() {
+
+    }
+}
