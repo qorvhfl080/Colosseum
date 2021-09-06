34b663cc5ba1f6e7def412d7903e863e03b88fda
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-06 15:06:30 +0900

    리스트뷰 어댑터 뼈대작업

---
+++ app/src/main/java/com/nepplus/colosseum/adapters/TopicAdapter.kt
@@ -0,0 +1,27 @@
+package com.nepplus.colosseum.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.nepplus.colosseum.R
+import com.nepplus.colosseum.datas.TopicData
+
+class TopicAdapter(val mContext: Context, resId: Int, val mList: List<TopicData>) : ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+        row!!
+
+
+
+        return row
+    }
+
+}
