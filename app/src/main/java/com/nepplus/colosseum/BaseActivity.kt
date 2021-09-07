a7133ad04f2658adcbe6f699a4784b1555e1b985
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-07 16:51:23 +0900

    supportActionBar가 존재시 커스텀 액션바 세팅

--- app/src/main/java/com/nepplus/colosseum/BaseActivity.kt
+++ app/src/main/java/com/nepplus/colosseum/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.nepplus.colosseum
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -12,10 +14,26 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
+
     }
 
     abstract fun setupEvents()
 
     abstract fun setValues()
+
+    fun setCustomActionBar() {
+
+        val defaultActionBar = supportActionBar!!
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val myToolbar = defaultActionBar.customView.parent as androidx.appcompat.widget.Toolbar
+        myToolbar.setContentInsetsAbsolute(0, 0)
+
+    }
 
 }
