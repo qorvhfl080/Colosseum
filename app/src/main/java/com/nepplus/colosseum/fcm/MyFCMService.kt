dd406207a3cfc1bd9f25298c88fc85c402f0c80c
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-08 11:17:34 +0900

    푸시알림 기초

---
+++ app/src/main/java/com/nepplus/colosseum/fcm/MyFCMService.kt
@@ -0,0 +1,15 @@
+package com.nepplus.colosseum.fcm
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFCMService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+        Log.d("push", "수신이벤트")
+    }
+
+}
