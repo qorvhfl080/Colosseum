a0145f7652824cb1dc3edae5de35db8918c6133b
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-08 12:09:14 +0900

    알림 목록 레이아웃 생성

---
+++ app/src/main/java/com/nepplus/colosseum/NotificationListActivity.kt
@@ -0,0 +1,20 @@
+package com.nepplus.colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_list)
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
