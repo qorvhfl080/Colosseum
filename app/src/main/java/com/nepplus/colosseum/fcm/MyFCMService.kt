02f49b0cc2975735d660754a14d61904416630dc
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-08 11:41:51 +0900

    핸들러 사용하여 토스트에 푸시 알림 내용 띄우기

--- app/src/main/java/com/nepplus/colosseum/fcm/MyFCMService.kt
+++ app/src/main/java/com/nepplus/colosseum/fcm/MyFCMService.kt
@@ -1,6 +1,9 @@
 package com.nepplus.colosseum.fcm
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -10,6 +13,15 @@ class MyFCMService : FirebaseMessagingService() {
         super.onMessageReceived(p0)
 
         Log.d("push", "수신이벤트")
+        
+        val title = p0.notification!!.title
+        val body = p0.notification!!.body
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.post {
+            Toast.makeText(applicationContext, "${body}", Toast.LENGTH_SHORT).show()
+        }
+        
     }
 
 }
