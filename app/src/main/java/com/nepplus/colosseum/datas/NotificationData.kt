4897f7864bdb1b60e21142c410dc5fffd692c6dd
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-08 15:36:53 +0900

    서버가 주는 시간을 parse하여 변수에 대입

--- app/src/main/java/com/nepplus/colosseum/datas/NotificationData.kt
+++ app/src/main/java/com/nepplus/colosseum/datas/NotificationData.kt
@@ -1,8 +1,12 @@
 package com.nepplus.colosseum.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NotificationData(var id: Int, var title: String) {
+
+    val createdAt = Calendar.getInstance()
 
     companion object {
 
@@ -12,6 +16,10 @@ class NotificationData(var id: Int, var title: String) {
 
             notificationData.id = json.getInt("id")
             notificationData.title = json.getString("title")
+
+            val createdAtString = json.getString("created_at")
+            val serverFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            notificationData.createdAt.time = serverFormat.parse(createdAtString)
 
             return notificationData
         }
