bff7cb7f28adfde52d2dc08c8299ccb5b085d4bd
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-09 11:54:41 +0900

    프로젝트에서 공유할 로그인 사용자 저장할 클래스 생성

---
+++ app/src/main/java/com/nepplus/colosseum/utils/GlobalData.kt
@@ -0,0 +1,13 @@
+package com.nepplus.colosseum.utils
+
+import com.nepplus.colosseum.datas.UserData
+
+class GlobalData {
+
+    companion object {
+
+        var loginUser: UserData? = null
+
+    }
+
+}
