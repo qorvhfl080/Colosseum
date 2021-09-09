9f6e157ff72af3eaffe9264657558484880fe0ae
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-09 15:19:01 +0900

    로그아웃 처리

--- app/src/main/java/com/nepplus/colosseum/MyProfileActivity.kt
+++ app/src/main/java/com/nepplus/colosseum/MyProfileActivity.kt
@@ -1,7 +1,13 @@
 package com.nepplus.colosseum
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.nepplus.colosseum.utils.ContextUtil
+import com.nepplus.colosseum.utils.GlobalData
+import kotlinx.android.synthetic.main.activity_my_profile.*
 
 class MyProfileActivity : BaseActivity() {
 
@@ -14,6 +20,33 @@ class MyProfileActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        logoutBtn.setOnClickListener {
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, position ->
+
+                ContextUtil.setToken(mContext, "")
+
+                GlobalData.loginUser = null
+
+                val myIntent = Intent(mContext, SignInActivity::class.java)
+                myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(myIntent)
+
+
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
+
+        quitBtn.setOnClickListener {
+
+
+
+        }
 
     }
 
